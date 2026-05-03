ac3b82dfed6e0c9f4ba8c72f2bb4e231267a21c3
Ranim <ranimzitouni13@gmail.com>
2026-05-03 00:02:27 +0000

    feat: improve login and register forms design

--- src/Entity/Recette.php
+++ src/Entity/Recette.php
@@ -2,12 +2,19 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use App\Entity\CategorieRecette;
 use App\Repository\RecetteRepository;
-use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
+#[ApiResource(
+    normalizationContext: ['groups' => ['recette:read']],
+    denormalizationContext: ['groups' => ['recette:write']]
+)]
 #[ORM\Entity(repositoryClass: RecetteRepository::class)]
 #[ORM\HasLifecycleCallbacks]
 class Recette
@@ -15,63 +22,71 @@ class Recette
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['recette:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
     #[Assert\NotBlank(message: 'Le titre est obligatoire.')]
     #[Assert\Length(min: 5, minMessage: 'Le titre doit avoir au moins 5 caractères.')]
+    #[Groups(['recette:read','recette:write'])]
     private ?string $titre = null;
 
     #[ORM\Column(type: 'text')]
     #[Assert\NotBlank(message: 'La description est obligatoire.')]
     #[Assert\Length(min: 30, minMessage: 'La description doit avoir au moins 30 caractères.')]
+    #[Groups(['recette:read','recette:write'])]
     private ?string $description = null;
 
     #[ORM\Column(type: 'text')]
     #[Assert\NotBlank(message: 'Les instructions sont obligatoires.')]
+    #[Groups(['recette:read','recette:write'])]
     private ?string $instructions = null;
 
     #[ORM\Column(type: 'integer')]
-    #[Assert\Range(min: 1, notInRangeMessage: 'Le temps de préparation doit être au moins 1 minute.')]
+    #[Assert\Range(min: 1)]
+    #[Groups(['recette:read','recette:write'])]
     private ?int $tempsPreparation = null;
 
     #[ORM\Column(type: 'integer', nullable: true)]
+    #[Groups(['recette:read','recette:write'])]
     private ?int $tempsCuisson = null;
 
     #[ORM\Column(length: 20)]
-    #[Assert\Choice(
-        choices: ['facile', 'moyen', 'difficile'],
-        message: 'La difficulté doit être : facile, moyen ou difficile.'
-    )]
+    #[Assert\Choice(choices: ['facile', 'moyen', 'difficile'])]
+    #[Groups(['recette:read','recette:write'])]
     private ?string $difficulte = null;
 
     #[ORM\Column(type: 'integer')]
-    #[Assert\Range(
-        min: 1,
-        max: 50,
-        notInRangeMessage: 'Le nombre de personnes doit être entre 1 et 50.'
-    )]
+    #[Assert\Range(min: 1, max: 50)]
+    #[Groups(['recette:read','recette:write'])]
     private ?int $nbPersonnes = null;
 
     #[ORM\Column(type: 'datetime')]
+    #[Groups(['recette:read'])]
     private ?\DateTimeInterface $dateCreation = null;
 
     #[ORM\Column(type: 'boolean')]
+    #[Groups(['recette:read'])]
     private ?bool $publiee = false;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['recette:read'])]
     private ?string $imageName = null;
 
     #[ORM\ManyToOne(inversedBy: 'recettes')]
+    #[Groups(['recette:read','recette:write'])]
     private ?CategorieRecette $categorie = null;
 
     #[ORM\ManyToOne(inversedBy: 'recettes')]
+    #[Groups(['recette:read'])]
     private ?User $auteur = null;
 
     #[ORM\ManyToMany(targetEntity: TagRecette::class, inversedBy: 'recettes')]
+    #[Groups(['recette:read','recette:write'])]
     private Collection $tags;
 
     #[ORM\OneToMany(mappedBy: 'recette', targetEntity: Ingredient::class, orphanRemoval: true)]
+    #[Groups(['recette:read','recette:write'])]
     private Collection $ingredients;
 
     public function __construct()
@@ -125,6 +140,7 @@ class Recette
     public function setAuteur(?User $auteur): static { $this->auteur = $auteur; return $this; }
 
     public function getTags(): Collection { return $this->tags; }
+
     public function addTag(TagRecette $tag): static
     {
         if (!$this->tags->contains($tag)) {
@@ -132,6 +148,7 @@ class Recette
         }
         return $this;
     }
+
     public function removeTag(TagRecette $tag): static
     {
         $this->tags->removeElement($tag);
@@ -139,6 +156,7 @@ class Recette
     }
 
     public function getIngredients(): Collection { return $this->ingredients; }
+
     public function addIngredient(Ingredient $ingredient): static
     {
         if (!$this->ingredients->contains($ingredient)) {
@@ -147,6 +165,7 @@ class Recette
         }
         return $this;
     }
+
     public function removeIngredient(Ingredient $ingredient): static
     {
         if ($this->ingredients->removeElement($ingredient)) {
